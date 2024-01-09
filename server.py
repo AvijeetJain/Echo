e1e2d9fccde9c2e0ddb52ff22daf06b8e8372a23
AvijeetJain <jainavijeet@gmail.com>
2024-01-09 19:12:57 +0530

    File Transfer Functionaly added server side

--- server.py
+++ server.py
@@ -2,18 +2,46 @@ import socket
 import threading
 
 
-def send_message(address, socket):
-    while True:
-        message = input("-> ")
-        print("Sending: " + message)
-        socket.sendto(message.encode('utf-8'), address)
 
-def receive_message(socket):
-    while True:
-        data, addr = socket.recvfrom(1024)
-        data = data.decode('utf-8')
-        print("Received from: " + str(addr))
-        print("From connected user: " + data)
+def send_file(s, server):
+    file_path = input("Enter the path of the file to send: ")
+
+    file_name = file_path.split("/")[-1]
+    s.sendto(file_name.encode('utf-8'), server)
+
+    with open(file_path, 'rb') as file:
+        data = file.read(1024)
+        while data:
+            s.sendto(data, server)
+            data = file.read(1024)
+    print("File sent successfully")
+
+def receive_file(socket):
+    file_name, addr = socket.recvfrom(1024)
+    file_name = file_name.decode('utf-8').strip()
+    print("Receiving file:", file_name)
+
+    with open(file_name, 'wb') as file:
+        while True:
+            data, addr = socket.recvfrom(1024)
+            if not data:
+                break
+            file.write(data)
+
+    print("File received successfully")
+
+# def send_message(address, socket):
+#     while True:
+#         message = input("-> ")
+#         print("Sending: " + message)
+#         socket.sendto(message.encode('utf-8'), address)
+
+# def receive_message(socket):
+#     while True:
+#         data, addr = socket.recvfrom(1024)
+#         data = data.decode('utf-8')
+#         print("Received from: " + str(addr))
+#         print("From connected user: " + data)
 
 
 def Main():
@@ -26,18 +54,22 @@ def Main():
     print("Server Started")
 
     client_address = ('192.168.171.59', 4005)  # Initialize client address
+    
+    data, addr = s.recvfrom(1024)
+    client_address = addr
 
+    receive_file(s)
+    send_file(client_address, s)
+    
+    # receive_thread = threading.Thread(target=receive_message, args=(s,))
+    # send_thread = threading.Thread(target=send_message, args=(client_address, s))
 
     
-    receive_thread = threading.Thread(target=receive_message, args=(s,))
-    send_thread = threading.Thread(target=send_message, args=(client_address, s))
+    # receive_thread.start()
+    # send_thread.start()
 
-    
-    receive_thread.start()
-    send_thread.start()
-
-    send_thread.join()
-    receive_thread.join()
+    # send_thread.join()
+    # receive_thread.join()
 
     s.close()
 
