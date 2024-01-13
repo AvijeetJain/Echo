1b67cc22cd3a013425c1b2962a8c20409d6d4728
Yuvraj Kumar <img_2021066@gmail.com>
2024-01-14 00:55:24 +0530

    initail branch

--- server.py
+++ server.py
@@ -4,31 +4,31 @@ import math
 import os
 
 
-def send_file(s, server):
-    file_path = input("Enter the path of the file to send: ")
+# def send_file(s, server):
+#     file_path = input("Enter the path of the file to send: ")
 
-    file_name = file_path.split("/")[-1]
-    s.sendto(file_name.encode('utf-8'), server)
+#     file_name = file_path.split("/")[-1]
+#     s.sendto(file_name.encode('utf-8'), server)
     
-    file_size = os.path.getsize(file_path)
-    file_size = file_size/10240
-    s.sendto(str(file_size).encode('utf-8'), server)
+#     file_size = os.path.getsize(file_path)
+#     file_size = file_size/10240
+#     s.sendto(str(file_size).encode('utf-8'), server)
 
-    with open(file_path, 'rb+') as file:
-        data = file.read(10240)
+#     with open(file_path, 'rb+') as file:
+#         data = file.read(10240)
         
-        while data:
-            s.sendto(data, server)
-            data = file.read(10240)
-        file.close()
-    print("File sent successfully")
+#         while data:
+#             s.sendto(data, server)
+#             data = file.read(10240)
+#         file.close()
+#     print("File sent successfully")
 
 def receive_file(socket):
-    file_name, addr = socket.recvfrom(1024)
+    file_name = socket.recv(1024)
     file_name = file_name.decode('utf-8').strip()
     print("Receiving file:", file_name)
 
-    file_size, addr = socket.recvfrom(1024)
+    file_size = socket.recv(1024)
     file_size = file_size.decode('utf-8').strip()
 
     file_size = math.ceil(float(file_size))
@@ -36,7 +36,7 @@ def receive_file(socket):
 
     with open(file_name, 'wb+') as file:
         for i in range(0,file_size):  
-            data, addr = socket.recvfrom(10240)          
+            data = socket.recv(10240)          
             # data = data.decode('utf-8').strip()
             file.write(data)
 
@@ -62,28 +62,31 @@ def Main():
     host = '192.168.137.1'  # Server ip
     port = 4000
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
+    s.listen(1)
+
+    client_socket, addr = s.accept()
 
     print("Server Started")
 
-    client_address = ('192.168.137.196', 4005)  # Initialize client address
+    # client_address = ('192.168.137.196', 4005)  # Initialize client address
     
     # data, addr = s.recvfrom(1024)
     # client_address = addr
 
     # send_file(s, client_address)
-    # receive_file(s)
+    receive_file(client_socket)
     
-    receive_thread = threading.Thread(target=receive_file, args=(s,))
-    send_thread = threading.Thread(target=send_file, args=(s, client_address))
+    # receive_thread = threading.Thread(target=receive_file, args=(client_socket,))
+    # send_thread = threading.Thread(target=send_file, args=(s, client_socket))
 
     
-    receive_thread.start()
-    send_thread.start()
+    # receive_thread.start()
+    # send_thread.start()
 
-    send_thread.join()
-    receive_thread.join()
+    # send_thread.join()
+    # receive_thread.join()
 
     s.close()
 
